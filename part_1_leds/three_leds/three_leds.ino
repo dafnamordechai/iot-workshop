297fac97435b5b7b5c17110303205cd9c0afc21d
dafnamordechai <dafna.mordechai@gmail.com>
2018-07-16 23:23:55 +0300

    iot-workshop files

---
+++ part_1_leds/three_leds/three_leds.ino
@@ -0,0 +1,35 @@
+int ledPin1 = 11;
+int ledPin2 = 12;
+
+void leds_setup(int ledPin1, int ledPin2)
+{
+  //Setup pin modes to ve output:
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+}
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  while (Serial.available()) {}
+  leds_setup(ledPin1, ledPin2);
+}
+
+void loop()
+{
+  static uint32_t cnt = 0;
+  Serial.print("loop() cnt:");
+  Serial.println(cnt); 
+  blink_led(ledPin1, 500);
+  blink_led(ledPin2, 500);
+  cnt++;
+}
+
+void blink_led(int led, uint32_t duration_ms)
+{
+    digitalWrite(led, HIGH);
+    delay(duration_ms);
+    digitalWrite(led, LOW);
+}
+
---
+++ part_5_photoresistor/PhotoResistor/PhotoResistor.ino
@@ -0,0 +1,52 @@
+
+int sensorPin = A0; //    
+int ledPin1 = 11;  
+int ledPin2 = 12;
+int ledPin3 = 13;
+int sensorValue = 0;  
+#define MIN_VAL 80
+#define MAX_VAL 700
+int treshhold_bins[] = {MAX_VAL/3, MAX_VAL*2/3, MAX_VAL};
+
+void leds_setup()
+{
+  //Setup pin modes to ve output:
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+}
+
+
+void setup() 
+{
+  Serial.begin(115200);
+  while (Serial.available()) {}
+  // declare the ledPin as an OUTPUT:
+  leds_setup();
+}
+
+void loop() 
+{
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  Serial.print("Sensor Value: ");
+  Serial.println(sensorValue);
+  
+  if(sensorValue < treshhold_bins[2]) 
+  {  digitalWrite(ledPin1, HIGH); }
+  else 
+  {  digitalWrite(ledPin1, LOW); }
+
+  if(sensorValue < treshhold_bins[1]) {
+    digitalWrite(ledPin2, HIGH); }
+  else { 
+    digitalWrite(ledPin2, LOW); }
+
+  if(sensorValue < treshhold_bins[0]) {
+    digitalWrite(ledPin3, HIGH); }
+  else { 
+    digitalWrite(ledPin3, LOW); }
+  
+  delay(500);
+    
+}
